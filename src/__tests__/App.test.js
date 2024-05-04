a5eca586352755674656c86330ae12d2914b1ab5
+ <+>
2024-05-04 15:27:15 +0300

    done

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,18 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text 'Hi I'm   ", () =>{
+render(<App/>)
+
+const topLevelHeading = screen.getByRole("heading", {
+    name: /hi, i'm/i,
+    exact: false,
+    level: 1,
+});
+
+expect(topLevelHeading).toBeInTheDocument();
+});
+
+
+
+
